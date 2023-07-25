b4ce34125838b9e5a3d3cd2f8a0b16f561d287b6
Full-Stack Guru <yuan59880@gmail.com>
2023-07-25 08:24:51 -0700

    fix start.sh

--- src/start.sh
+++ src/start.sh
@@ -6,7 +6,7 @@ echo "Starting WebUI API"
 python /stable-diffusion-webui/webui.py --skip-python-version-check --skip-torch-cuda-test --no-tests --skip-install --lowram --opt-sdp-attention --disable-safe-unpickle --port 3000 --api --nowebui --skip-version-check  --no-hashing --no-download-sd-model &
 
 echo "Starting LoRA API"
-python /kohya_ss_api/lora_api.py &
+cd /kohya_ss_api && python lora_api.py &
 
 echo "Starting RunPod Handler"
 python -u /rp_handler.py
